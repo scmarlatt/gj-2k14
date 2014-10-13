ec3f65ed51c23dccb355152ca8544423499f23bb
scmarlatt <smarlatt@umich.edu>
2014-10-12 23:49:29 -0400

    Scooters animations

---
+++ GameJam/Assets/Scripts/playerStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerStats : MonoBehaviour {
+
+	static public playerStats ps;
+
+	public float health = 100;
+	public float mana = 60f;
+	public float manaRegen = 5.0f;
+	public float armor = 0.0f;
+	public int ltResource = 0;
+	public int wtResource = 0;
+	public int etResource = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GameJam/Assets/Scripts/sunmanController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sunmanController : MonoBehaviour {
+
+	public float maxY = 5f;
+	public float maxSpeed = 10f;
+	private bool facingRight = true;
+	private int rfs = 0;
+
+	Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float move = Input.GetAxis ("Horizontal");
+		rigidbody.velocity = new Vector3 (move * maxSpeed, rigidbody.velocity.y, 0);
+
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		if (move > 0 && !facingRight) {
+			Flip ();
+		}
+		else if(move < 0 && facingRight){
+			Flip ();
+		}
+
+		if (Input.GetKeyDown ("z")) {
+			rigidbody.velocity = new Vector3 (rigidbody.velocity.x, maxY, 0);
+		}
+		if (rigidbody.velocity.y > 0) {
+			rfs = 1;		
+		}
+		else if(rigidbody.velocity.y == 0){
+			rfs = 0;
+		}
+		else if(rigidbody.velocity.y < 0){
+			rfs = -1;
+		}
+
+
+		anim.SetInteger ("Aerials", rfs);
+
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnCollisionEnter(Collider col){
+		if (col.gameObject.layer == 9) {
+			rfs = 0;
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x, 0,0);		
+		}
+	}
+}
