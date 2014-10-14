02f5d04e28b13f2004a7300e0557de4af5fa58c5
Alex <alchkim@umich.edu>
2014-10-13 22:54:07 -0400

    Added ways to enter other worlds from other places

---
+++ GameJam/Assets/GameBackgrounds/Scripts/NewArea.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewArea : MonoBehaviour {
+
+	public enum Location { Water, Lightning, Earth, Base };
+	public Location loc;
+	
+	void OnTriggerEnter(Collider coll) {
+		Debug.Log ("yo");
+		if(coll.gameObject.name == "sunman") {
+			if(loc == Location.Water) {
+				Application.LoadLevel ("Water_World");
+			} else if (loc == Location.Lightning) {
+				Application.LoadLevel ("Thunder_World");
+			} else if (loc == Location.Earth) {
+				Application.LoadLevel ("Earth_World");
+			} else if (loc == Location.Base) {
+				Application.LoadLevel ("Main_Tower");
+			}
+		}
+	}
+}
